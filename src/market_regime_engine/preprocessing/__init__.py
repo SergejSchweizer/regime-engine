d8061d875d91879b7df13b2a0ef900707972ccc3
SSC <sergej.schweizer@gmail.com>
2026-08-24 10:07:52 +0200

    feat(PR-009-preprocessing-pipeline): export preprocessing API

---
+++ src/market_regime_engine/preprocessing/__init__.py
@@ -0,0 +1,5 @@
+"""Train-only preprocessing primitives."""
+
+from market_regime_engine.preprocessing.scaling import StandardScalerArtifact, fit_standard_scaler
+
+__all__ = ["StandardScalerArtifact", "fit_standard_scaler"]
