0a38539ec4087ad3c1f902bf01ccab063c2c3190
SSC <sergej.schweizer@gmail.com>
2026-08-24 10:09:17 +0200

    feat(PR-011-prediction-store): export prediction store API

---
+++ src/market_regime_engine/predictions/__init__.py
@@ -0,0 +1,5 @@
+"""Immutable prediction build storage."""
+
+from market_regime_engine.predictions.store import PredictionBuildManifest, PredictionStore
+
+__all__ = ["PredictionBuildManifest", "PredictionStore"]
