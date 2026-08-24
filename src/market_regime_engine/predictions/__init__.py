da74402a407c0482dad8a6494e3f667c2fa3ac0a
SSC <sergej.schweizer@gmail.com>
2026-08-24 10:30:41 +0200

    style(PR-011-prediction-store): format prediction exports

--- src/market_regime_engine/predictions/__init__.py
+++ src/market_regime_engine/predictions/__init__.py
@@ -1,5 +1,8 @@
 """Immutable prediction build storage."""
 
-from market_regime_engine.predictions.store import PredictionBuildManifest, PredictionStore
+from market_regime_engine.predictions.store import (
+    PredictionBuildManifest,
+    PredictionStore,
+)
 
 __all__ = ["PredictionBuildManifest", "PredictionStore"]
